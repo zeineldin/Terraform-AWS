b006f0573b011b499a347667abf263887f8e9a46
Mohamed Zeineldin <eng.mohamed.zeineldin@gmail.com>
2018-11-12 22:14:07 +0100

    edit the credential file

--- provider.tf
+++ provider.tf
@@ -3,6 +3,7 @@ provider "aws" {
   region                  = "eu-central-1"
 #  access_key = "${AWS_ACCESS}"
 #  secret_key = "${AWS_SECRET}"
+  shared_credentials_file = "/Users/<USER_NAME>/.aws/credentials"
   profile                 = "default"
 
 }
