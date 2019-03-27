4f2aaa9f4840f5fd686728f4730b6f93641258b0
zeineldin <eng.mohamed.zeineldin@gmail.com>
2019-03-27 18:04:19 +0100

    adding the second case study

---
+++ case-study-2/scrpt.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+yum update
+yum -y install nginx
---
+++ case-study-2/vars.tf
@@ -0,0 +1,23 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-844e0bf7"
+  }
+}
+
+variable "PATH_TO_PRIVATE_SSH_KEY" {
+  default = "mykey"
+}
+variable "PATH_TO_PUBLIC_SSH_KEY" {
+  default = "mykey.pub"
+}
+variable "EC2_INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
