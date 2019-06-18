94ca8c80ff9f73068efcccfee26589a3a51b268e
zeineldin <eng.mohamed.zeineldin@gmail.com>
2019-06-18 12:07:52 +0200

    add new user-cases

---
+++ case-study-z/instance.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  #access_key = "ACCESS_KEY_HERE"
+  #secret_key = "SECRET_KEY_HERE"
+  shared_credentials_file = "~/.aws/credentials"
+  region     = "eu-west-1"
+}
+
+resource "aws_instance" "devopsarea" {
+  ami           = "ami-08935252a36e25f85"
+  instance_type = "t2.micro"
+
+tags {
+    "Name" = "devopsarea-instance"
+  }
+}
