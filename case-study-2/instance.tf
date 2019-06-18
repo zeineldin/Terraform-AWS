40d8ce85d6e303c04b1844b458f27176ba0f2660
Mohamed Zeineldin <eng.mohamed.zeineldin@gmail.com>
2019-06-18 12:53:00 +0200

    Update instance.tf

--- case-study-2/instance.tf
+++ case-study-2/instance.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "mykey" {
+resource "aws_key_pair" "devops_key" {
   key_name = "mykey"
   public_key = "${file("${var.PATH_TO_PUBLIC_SSH_KEY}")}"
 }
@@ -6,7 +6,7 @@ resource "aws_key_pair" "mykey" {
 resource "aws_instance" "example" {
   ami = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
-  key_name = "${aws_key_pair.mykey.key_name}"
+  key_name = "${aws_key_pair.devops_key.key_name}"
 
   provisioner "file" {
     source = "script.sh"
